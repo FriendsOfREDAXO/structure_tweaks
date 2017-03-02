4146250dfb1aca9b90e3e82541db271f0431ae9f
DanielWeitenauer <github@studio-ahoi.de>
2017-03-02 10:26:36 +0100

    Fix adding of new column

--- update.php
+++ update.php
@@ -3,9 +3,7 @@
  * @var rex_addon $this
  */
 
-// New column in beta version
-if (rex_string::versionCompare($this->getVersion(), '1.0.0-beta', '<')) {
-    rex_sql_table::get(rex::getTable('structure_tweaks'))
-        ->ensureColumn(new rex_sql_column('label', 'varchar(100)', false))
-        ->alter();
-}
+// New column in version 1.0.0-beta
+rex_sql_table::get(rex::getTable('structure_tweaks'))
+    ->ensureColumn(new rex_sql_column('label', 'varchar(100)', false))
+    ->alter();
