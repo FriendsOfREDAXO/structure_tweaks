ec4de47328aaa72547a2a87fe563f1f6dc871bf8
Daniel <github@studio-ahoi.de>
2017-03-02 10:48:42 +0100

    Fix install, update and uninstall (#14)
    * Remove table on uninstall
    * Fix adding of new column

--- install.php
+++ install.php
@@ -3,8 +3,9 @@
  * @var rex_addon $this
  */
 
-// New column in beta version
-if (rex_string::versionCompare($this->getVersion(), '1.0.0-beta', '<')) {
+// New column in version 1.0.0-beta
+$tables = rex_sql::showTables();
+if (in_array(rex::getTable('structure_tweaks'), $tables)) {
     rex_sql_table::get(rex::getTable('structure_tweaks'))
         ->ensureColumn(new rex_sql_column('label', 'varchar(100)', false))
         ->alter();
---
+++ uninstall.sql
@@ -0,0 +1,1 @@
+DROP TABLE IF EXISTS `%TABLE_PREFIX%structure_tweaks`;
