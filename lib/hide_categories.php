a64eee17ab24c3b7b4f126456c8bdc545812bfeb
DanielWeitenauer <github@studio-ahoi.de>
2020-10-30 13:40:24 +0100

    Hide categories in functions tab, closes #63

--- lib/hide_categories.php
+++ lib/hide_categories.php
@@ -17,6 +17,10 @@ class structure_tweaks_hide_categories extends structure_tweaks_base
                 rex_extension::register('PAGE_HEADER', [__CLASS__, 'ep']);
             }
         });
+
+        if (rex_plugin::get('structure', 'content')->isAvailable() && rex_request('page', 'string') == 'content/functions') {
+            rex_extension::register('OUTPUT_FILTER', [__CLASS__, 'epFunctions']);
+        }
     }
 
     /**
@@ -47,8 +51,8 @@ class structure_tweaks_hide_categories extends structure_tweaks_base
         $hidden_categories = self::getHiddenCategories();
         if (!empty($hidden_categories)) {
             if (rex_request('page', 'string') == 'structure') {
-            $subject .= self::getScript($hidden_categories);
-        }
+                $subject .= self::getScript($hidden_categories);
+            }
             if (rex_request('page', 'string') == 'linkmap') {
                 $subject .= self::getScriptInLinkmap($hidden_categories);
             }
@@ -58,11 +62,37 @@ class structure_tweaks_hide_categories extends structure_tweaks_base
         $hidden_categories = self::getHiddenCategories(true);
         if (!empty($hidden_categories) && !rex::getUser()->isAdmin()) {
             if (rex_request('page', 'string') == 'structure') {
-            $subject .= self::getScript($hidden_categories);
-        }
+                $subject .= self::getScript($hidden_categories);
+            }
             if (rex_request('page', 'string') == 'linkmap') {
                 $subject .= self::getScriptInLinkmap($hidden_categories);
             }
+        }
+
+        return $subject;
+    }
+
+    /**
+     * EP CALLBACK
+     * @param rex_extension_point $ep
+     * @return string
+     */
+    public static function epFunctions(rex_extension_point $ep)
+    {
+        $subject = $ep->getSubject();
+
+        // Hidden categories
+        $hidden_categories = self::getHiddenCategories();
+        if (!empty($hidden_categories)) {
+            $subject = self::removeCategoryOptions($subject, 'category_id_new', $hidden_categories);
+            $subject = self::removeCategoryOptions($subject, 'category_copy_id_new', $hidden_categories);
+        }
+
+        // Hidden non-admin categories
+        $hidden_categories = self::getHiddenCategories(true);
+        if (!empty($hidden_categories) && !rex::getUser()->isAdmin()) {
+            $subject = self::removeCategoryOptions($subject, 'category_id_new', $hidden_categories);
+            $subject = self::removeCategoryOptions($subject, 'category_copy_id_new', $hidden_categories);
         }
 
         return $subject;
@@ -100,5 +130,27 @@ class structure_tweaks_hide_categories extends structure_tweaks_base
                 });
             </script>
         ';
+    }
+
+    private static function removeCategoryOptions(string $subject, string $select_id, array $hidden_categories): string
+    {
+        libxml_use_internal_errors(true); // Disable HTML parsing warnings @see https://stackoverflow.com/questions/9149180/domdocumentloadhtml-error
+
+        $dom = new DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($subject);
+
+        $element = $dom->getElementById($select_id);
+        if ($element) {
+            /** @var DOMElement $option */
+            foreach ($element->getElementsByTagName('option') as $option) {
+                if (in_array($option->getAttribute('value'), $hidden_categories)) {
+                    $element->removeChild($option);
+                }
+            }
+        }
+
+        libxml_use_internal_errors(false); // Enable HTML parsing warnings
+
+        return $dom->saveHTML();
     }
 }
